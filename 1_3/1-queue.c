08db37f38feae76195adfbf2af24f850f099642d
SaiMohan-28 <98573929+SaiMohan-28@users.noreply.github.com>
2022-03-01 20:19:52 +0530

    queue

---
+++ 1_3/1-queue.c
@@ -0,0 +1,74 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct queue
+{
+	int data;
+	struct queue *link;
+}q;
+void enqueue(int data,q **rear_arg,q **front_arg)
+{
+	q *new=NULL;
+	new=(q *)malloc(sizeof(q));
+	if(new==NULL)
+	printf("mem is not allocated\n");
+	else
+	{
+		new->data=data;
+		new->link=NULL;
+		if(*rear_arg==NULL && *front_arg==NULL)
+		{
+			*rear_arg=new;
+			*front_arg=new;
+		}
+		else
+		{
+			(*rear_arg)->link=new;
+			*rear_arg=new;
+		}
+	}
+	printf("enqued element is %d\n",(*rear_arg)->data);
+}
+void display(q **front_arg)
+{
+	if(*front_arg==NULL)
+	printf("Queue is empty\n");
+	else
+	{
+		q *temp=*front_arg;
+		while(temp!=NULL)
+		{
+			printf("element is %d\n",temp->data);
+			temp=temp->link;
+		}
+		free(temp);
+	}
+}
+void dequeue(q **front_arg)
+{
+	q *temp=*front_arg;
+	if(temp==NULL)
+	{
+		printf("queue is empty\n");
+	}
+	else
+	{
+		printf("dequed element is %d\n",temp->data);
+		*front_arg=temp->link;
+	}
+	free(temp);
+}
+int main()
+{
+	q *rear=NULL,*front=NULL;
+	enqueue(10,&rear,&front);
+	enqueue(20,&rear,&front);
+	enqueue(30,&rear,&front);
+	display(&front);
+	dequeue(&front);
+	dequeue(&front);
+	dequeue(&front);
+	dequeue(&front);
+	display(&front);
+	enqueue(10,&rear,&front);
+	return 0;
+}
